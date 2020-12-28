fa397925729650c8284ca82cf2c462348a2b0b75
parker <parker@redso.com.hk>
2020-12-28 17:06:27 +0800

    glsl typesetting

--- dist/ripples-OOP.js
+++ dist/ripples-OOP.js
@@ -513,7 +513,7 @@ class Ripples {
     // Here the 'container' is the element which the background adapts to
     // (either the chrome window or some element, depending on attachment)
     let container;
-    if (backgroundAttachment == 'fixed') {
+    if (backgroundAttachment === 'fixed') {
       container = { left: window.pageXOffset, top: window.pageYOffset };
       container.width = window.innerWidth;
       container.height = window.innerHeight;
@@ -612,105 +612,104 @@ class Ripples {
   }
 
   initShaders() {
-    const vertexShader = [
-      'attribute vec2 vertex;',
-      'varying vec2 coord;',
-      'void main() {',
-      'coord = vertex * 0.5 + 0.5;',
-      'gl_Position = vec4(vertex, 0.0, 1.0);',
-      '}'
-    ].join('\n');
+    const vertexShader = `
+      attribute vec2 vertex;
+      varying vec2 coord;
+      void main() {
+        coord = vertex * 0.5 + 0.5;
+        gl_Position = vec4(vertex, 0.0, 1.0);
+      }
+    `;
 
-    this.dropProgram = this.createProgram(vertexShader, [
-      'precision highp float;',
+    this.dropProgram = this.createProgram(vertexShader,
+      `precision highp float;
 
-      'const float PI = 3.141592653589793;',
-      'uniform sampler2D texture;',
-      'uniform vec2 center;',
-      'uniform float radius;',
-      'uniform float strength;',
+      const float PI = 3.141592653589793;
+      uniform sampler2D texture;
+      uniform vec2 center;
+      uniform float radius;
+      uniform float strength;
 
-      'varying vec2 coord;',
+      varying vec2 coord;
 
-      'void main() {',
-      'vec4 info = texture2D(texture, coord);',
+      void main() {
+        vec4 info = texture2D(texture, coord);
 
-      'float drop = max(0.0, 1.0 - length(center * 0.5 + 0.5 - coord) / radius);',
-      'drop = 0.5 - cos(drop * PI) * 0.5;',
+        float drop = max(0.0, 1.0 - length(center * 0.5 + 0.5 - coord) / radius);
+        drop = 0.5 - cos(drop * PI) * 0.5;
 
-      'info.r += drop * strength;',
+        info.r += drop * strength;
 
-      'gl_FragColor = info;',
-      '}'
-    ].join('\n'));
+        gl_FragColor = info;
+      }`
+    );
 
-    this.updateProgram = this.createProgram(vertexShader, [
-      'precision highp float;',
+    this.updateProgram = this.createProgram(vertexShader,
+      `precision highp float;
 
-      'uniform sampler2D texture;',
-      'uniform vec2 delta;',
+      uniform sampler2D texture;
+      uniform vec2 delta;
 
-      'varying vec2 coord;',
+      varying vec2 coord;
 
-      'void main() {',
-      'vec4 info = texture2D(texture, coord);',
+      void main() {
+        vec4 info = texture2D(texture, coord);
 
-      'vec2 dx = vec2(delta.x, 0.0);',
-      'vec2 dy = vec2(0.0, delta.y);',
+        vec2 dx = vec2(delta.x, 0.0);
+        vec2 dy = vec2(0.0, delta.y);
 
-      'float average = (',
-      'texture2D(texture, coord - dx).r +',
-      'texture2D(texture, coord - dy).r +',
-      'texture2D(texture, coord + dx).r +',
-      'texture2D(texture, coord + dy).r',
-      ') * 0.25;',
+        float average = (
+          texture2D(texture, coord - dx).r +
+          texture2D(texture, coord - dy).r +
+          texture2D(texture, coord + dx).r +
+          texture2D(texture, coord + dy).r
+        ) * 0.25;
 
-      'info.g += (average - info.r) * 2.0;',
-      'info.g *= 0.995;',
-      'info.r += info.g;',
+        info.g += (average - info.r) * 2.0;
+        info.g *= 0.995;
+        info.r += info.g;
 
-      'gl_FragColor = info;',
-      '}'
-    ].join('\n'));
+        gl_FragColor = info;
+      }`
+    );
     Ripples.gl.uniform2fv(this.updateProgram.locations.delta, this.textureDelta);
 
-    this.renderProgram = this.createProgram([
-      'precision highp float;',
+    this.renderProgram = this.createProgram(
+      `precision highp float;
 
-      'attribute vec2 vertex;',
-      'uniform vec2 topLeft;',
-      'uniform vec2 bottomRight;',
-      'uniform vec2 containerRatio;',
-      'varying vec2 ripplesCoord;',
-      'varying vec2 backgroundCoord;',
-      'void main() {',
-      'backgroundCoord = mix(topLeft, bottomRight, vertex * 0.5 + 0.5);',
-      'backgroundCoord.y = 1.0 - backgroundCoord.y;',
-      'ripplesCoord = vec2(vertex.x, -vertex.y) * containerRatio * 0.5 + 0.5;',
-      'gl_Position = vec4(vertex.x, -vertex.y, 0.0, 1.0);',
-      '}'
-    ].join('\n'), [
-      'precision highp float;',
+      attribute vec2 vertex;
+      uniform vec2 topLeft;
+      uniform vec2 bottomRight;
+      uniform vec2 containerRatio;
+      varying vec2 ripplesCoord;
+      varying vec2 backgroundCoord;
+      void main() {
+        backgroundCoord = mix(topLeft, bottomRight, vertex * 0.5 + 0.5);
+        backgroundCoord.y = 1.0 - backgroundCoord.y;
+        ripplesCoord = vec2(vertex.x, -vertex.y) * containerRatio * 0.5 + 0.5;
+        gl_Position = vec4(vertex.x, -vertex.y, 0.0, 1.0);
+      }`,
+      `precision highp float;
 
-      'uniform sampler2D samplerBackground;',
-      'uniform sampler2D samplerRipples;',
-      'uniform vec2 delta;',
+      uniform sampler2D samplerBackground;
+      uniform sampler2D samplerRipples;
+      uniform vec2 delta;
 
-      'uniform float perturbance;',
-      'varying vec2 ripplesCoord;',
-      'varying vec2 backgroundCoord;',
+      uniform float perturbance;
+      varying vec2 ripplesCoord;
+      varying vec2 backgroundCoord;
 
-      'void main() {',
-      'float height = texture2D(samplerRipples, ripplesCoord).r;',
-      'float heightX = texture2D(samplerRipples, vec2(ripplesCoord.x + delta.x, ripplesCoord.y)).r;',
-      'float heightY = texture2D(samplerRipples, vec2(ripplesCoord.x, ripplesCoord.y + delta.y)).r;',
-      'vec3 dx = vec3(delta.x, heightX - height, 0.0);',
-      'vec3 dy = vec3(0.0, heightY - height, delta.y);',
-      'vec2 offset = -normalize(cross(dy, dx)).xz;',
-      'float specular = pow(max(0.0, dot(offset, normalize(vec2(-0.6, 1.0)))), 4.0);',
-      'gl_FragColor = texture2D(samplerBackground, backgroundCoord + offset * perturbance) + specular;',
-      '}'
-    ].join('\n'));
+      void main() {
+        float height = texture2D(samplerRipples, ripplesCoord).r;
+        float heightX = texture2D(samplerRipples, vec2(ripplesCoord.x + delta.x, ripplesCoord.y)).r;
+        float heightY = texture2D(samplerRipples, vec2(ripplesCoord.x, ripplesCoord.y + delta.y)).r;
+        vec3 dx = vec3(delta.x, heightX - height, 0.0);
+        vec3 dy = vec3(0.0, heightY - height, delta.y);
+        vec2 offset = -normalize(cross(dy, dx)).xz;
+        float specular = pow(max(0.0, dot(offset, normalize(vec2(-0.6, 1.0)))), 4.0);
+        gl_FragColor = texture2D(samplerBackground, backgroundCoord + offset * perturbance) + specular;
+      }`
+    );
     Ripples.gl.uniform2fv(this.renderProgram.locations.delta, this.textureDelta);
   }
 
@@ -788,7 +787,7 @@ class Ripples {
       (elHeight - 2 * y) / longestSide
     ]);
 
-    Ripples.gl.viewport(0, 0, this.resolution, this.resolution);
+    Ripples.gl.viewport(0, 0, this.resolution, this.resolution); // 設置畫布的起始座標與寬高
 
     Ripples.gl.bindFramebuffer(Ripples.gl.FRAMEBUFFER, this.framebuffers[this.bufferWriteIndex]);
     this.bindTexture(this.textures[this.bufferReadIndex]);
